5e6d087f62ead549274e0dcd3b9ea1ab5b137b33
TJ VanToll <tj.vantoll@gmail.com>
2015-10-13 15:32:00 -0400

    Dynamically navigate to page 2

--- app/page-1.js
+++ app/page-1.js
@@ -1,11 +1,21 @@
 var frameModule = require("ui/frame");
 var gesturesModule = require("ui/gestures");
+var pageModule = require("ui/page");
+var labelModule = require("ui/label");
 
 exports.loaded = function(args) {
 	var page = args.object;
 	page.on(gesturesModule.GestureTypes.swipe, function(args) {
 		if (args.direction == gesturesModule.SwipeDirection.left) {
-			frameModule.topmost().navigate("page-2");
+			frameModule.topmost().navigate({
+				create: function() {
+					var newPage = new pageModule.Page();
+					var label = new labelModule.Label();
+					label.text = "page 2";
+					newPage.content = label;
+					return newPage;
+				}
+			});
 		}
 	});
 };
